3ed735ccf66d83aa6a720d421ddb5cc4697e18e1
Ryosuke <ryosuke.san.hana@gmail.com>
2020-09-17 13:51:47 -0700

    native component test

--- components/ComponentName.jsx
+++ components/ComponentName.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { View, Text } from "react-native";
 
 export default function ComponentName() {
-  return <div>Test Component</div>;
+  return (
+    <View>
+      <Text>Test Component</Text>
+    </View>
+  );
 }
